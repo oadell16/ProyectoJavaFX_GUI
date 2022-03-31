2a1599101206582cf43595475ddab13c0c785388
oadell16 <oadell@ibadia.cat>
2022-04-01 00:25:40 +0200

    packs acabados

--- src/controlador/ProductsController.java
+++ src/controlador/ProductsController.java
@@ -3,6 +3,8 @@ package controlador;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 import org.controlsfx.validation.Severity;
 import org.controlsfx.validation.ValidationSupport;
@@ -18,6 +20,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import model.Pack;
 import model.Product;
 import model.ProductDAO;
 
@@ -59,6 +62,7 @@ public class ProductsController{
 		vs.registerValidator(stockTextField, true, Validator.createEmptyValidator("Stock obligatori"));
 		vs.registerValidator(initialDateTextField, true, Validator.createEmptyValidator("Fecha inicial del producto obligatori"));
 		vs.registerValidator(finalDateTextField, true, Validator.createEmptyValidator("Fecha final del producto obligatori"));
+
         //https://howtodoinjava.com/regex/java-regex-validate-email-address/
 	}
 
@@ -74,7 +78,17 @@ public class ProductsController{
 		if (checkPack.isSelected()) {
 			idProductsTextArea.setDisable(false);
 			dtoTextField.setDisable(false);
+			vs.registerValidator(idProductsTextArea, true, Validator.createEmptyValidator("ids dels productes obligatori"));
+			vs.registerValidator(dtoTextField, true, Validator.createEmptyValidator("Descompte obligatori"));
 		}else{
+			vs = new ValidationSupport();
+			vs.registerValidator(idTextField, true, Validator.createEmptyValidator("ID obligatori"));
+			vs.registerValidator(nomTextField, true, Validator.createEmptyValidator("Nom obligatori"));
+			vs.registerValidator(priceTextField, true, Validator.createEmptyValidator("Preu obligatori"));
+			vs.registerValidator(stockTextField, true, Validator.createEmptyValidator("Stock obligatori"));
+			vs.registerValidator(initialDateTextField, true, Validator.createEmptyValidator("Fecha inicial del producto obligatori"));
+			vs.registerValidator(finalDateTextField, true, Validator.createEmptyValidator("Fecha final del producto obligatori"));
+
 			idProductsTextArea.setDisable(true);
 			dtoTextField.setDisable(true);
 		}
@@ -82,40 +96,89 @@ public class ProductsController{
 	}
 
 	@FXML private void onKeyPressedId(KeyEvent e) throws IOException {
-
 		if (e.getCode() == KeyCode.ENTER || e.getCode() == KeyCode.TAB){
-			//Comprovar si existeix la persona indicada en el control idTextField
-			Product product = products.search(Integer.parseInt(idTextField.getText()));
-			if(product != null){
-				//posar els valors per modificarlos
-				nomTextField.setText(product.getName());
-				priceTextField.setText(product.getSalePrice().toString());
-				stockTextField.setText(product.getStock().toString());
-				stockTextField.setDisable(true);
-				initialDateTextField.setValue(product.getInitialCatalogDate());
-				finalDateTextField.setValue(product.getEndCatalogDate());
-			}else{ 
-				//nou registre
-				nomTextField.setText("");
-				priceTextField.setText("");
-				stockTextField.setText("");
-				stockTextField.setDisable(false);
-				initialDateTextField.setValue(null);
-				finalDateTextField.setValue(null);
-			}
+				
+				if (products.search(Integer.parseInt(idTextField.getText())) instanceof Product) {
+					//Comprovar si existeix la persona indicada en el control idTextField
+					Product product = products.search(Integer.parseInt(idTextField.getText()));
+					
+					//posar els valors per modificarlos
+					nomTextField.setText(product.getName());
+					priceTextField.setText(product.getSalePrice().toString());
+					stockTextField.setText(product.getStock().toString());
+					stockTextField.setDisable(true);
+					initialDateTextField.setValue(product.getInitialCatalogDate());
+					finalDateTextField.setValue(product.getEndCatalogDate());
+					
+				}else if(products.search(Integer.parseInt(idTextField.getText())) instanceof Pack){
+					Pack pack = (Pack)products.search(Integer.parseInt(idTextField.getText()));
+
+					nomTextField.setText(pack.getName());
+					priceTextField.setText(pack.getSalePrice().toString());
+					stockTextField.setText(pack.getStock().toString());
+					stockTextField.setDisable(true);
+					initialDateTextField.setValue(pack.getInitialCatalogDate());
+					finalDateTextField.setValue(pack.getEndCatalogDate());
+
+					idProductsTextArea.setText(pack.getIds().toString());
+					dtoTextField.setText(pack.getPercentageDiscount().toString());
+
+				}else{ 
+					//nou registre
+					nomTextField.setText("");
+					priceTextField.setText("");
+					stockTextField.setText("");
+					stockTextField.setDisable(false);
+					initialDateTextField.setValue(null);
+					finalDateTextField.setValue(null);
+					idProductsTextArea.setText("");
+					dtoTextField.setText("");
+				}
 		}
+		
 	}
 	 
 	@FXML private void onActionGuardar(ActionEvent e) throws IOException {
 		//verificar si les dades són vàlides				
 		if(isDatosValidos()){
-			Product product = new Product(Integer.parseInt(idTextField.getText()), nomTextField.getText(),Integer.parseInt(priceTextField.getText()),
-			Integer.parseInt(stockTextField.getText()),initialDateTextField.getValue(),finalDateTextField.getValue());
-			
-			
-			products.add(product);
-			limpiarFormulario();
-			System.out.println(products.getMap());
+			if (products.search(Integer.parseInt(idTextField.getText())) instanceof Product) {
+				Product product = new Product(Integer.parseInt(idTextField.getText()), nomTextField.getText(),Integer.parseInt(priceTextField.getText()),
+				Integer.parseInt(stockTextField.getText()),initialDateTextField.getValue(),finalDateTextField.getValue());
+				
+				
+				products.add(product);
+				limpiarFormulario();
+				System.out.println(products.getMap());
+			}else{
+				TreeSet<Product> ids = new TreeSet<Product>();
+				String[] idsProducts = idProductsTextArea.getText().split(",");
+
+				for (String id : idsProducts) {
+					if (products.search(Integer.parseInt(id))!=null) {
+						ids.add(products.search(Integer.parseInt(id)));
+					}
+				}
+
+				if (ids!=null) {
+					Pack pack = new Pack(
+					Integer.parseInt(idTextField.getText()), 
+					nomTextField.getText(),
+					Integer.parseInt(priceTextField.getText()),
+					Integer.parseInt(stockTextField.getText()),
+					initialDateTextField.getValue(),
+					finalDateTextField.getValue(),
+					ids,
+					Integer.parseInt(dtoTextField.getText())
+					);
+
+					products.add(pack);
+
+
+				}
+				
+				limpiarFormulario();
+				System.out.println(products.getMap().toString());
+			}
 		}
 	}
 
@@ -168,5 +231,7 @@ public class ProductsController{
 		stockTextField.setText("");
 		initialDateTextField.setValue(null);
 		finalDateTextField.setValue(null);
+		idProductsTextArea.setText("");
+		dtoTextField.setText("");
 	}
 }
--- src/model/Pack.java
+++ src/model/Pack.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.time.LocalDate;
 import java.util.TreeSet;
 
@@ -5,14 +7,18 @@ public final class Pack extends Product{
     private TreeSet<Product> ids;
     private int percentageDiscount;
 
-    public Pack(int idProduct, String name, int salePrice, int stock, LocalDate initialCatalogDate, LocalDate endCatalogDate, TreeSet<Product> ids, int percentageDiscount) {
+    public Pack(Integer idProduct, String name, int salePrice, int stock, LocalDate initialCatalogDate, LocalDate endCatalogDate, TreeSet<Product> ids, int percentageDiscount) {
         super(idProduct, name, salePrice, stock, initialCatalogDate, endCatalogDate);
         this.ids = ids;
         this.percentageDiscount = percentageDiscount;
     }
 
-    public int getPercentageDiscount() {
+    public Integer getPercentageDiscount() {
         return percentageDiscount;
+    }
+    
+    public TreeSet<Product> getIds() {
+        return this.ids;
     }
 
     public void setPercentageDiscount(int percentageDiscount) {
@@ -27,7 +33,7 @@ public final class Pack extends Product{
 
     @Override
     public String toString() {
-        return super.toString()+"Pack [Productos del pack=" + ids + ", Descuento del pack=" + percentageDiscount + "]";
+        return super.toString()+", Pack [Productos del pack=" + ids + ", Descuento del pack=" + percentageDiscount + "]";
     }
     @Override
     public boolean equals(Object obj) {
